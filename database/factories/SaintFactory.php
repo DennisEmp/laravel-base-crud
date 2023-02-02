57b6a0ad02c6943572c8d9a66bf4df449ab6f85b
Dennis Empirio <dennis.empirio@gmail.com>
2023-02-02 16:04:24 +0100

    first commit

---
+++ database/factories/SaintFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class SaintFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+
+            'name' => fake() -> name(),
+            'birth_place' => fake() -> words(),
+            'blessing_date' => fake() -> dateTime(),
+            'miracles' => fake() -> numberBetween(0, 30),
+
+        ];
+    }
+}
