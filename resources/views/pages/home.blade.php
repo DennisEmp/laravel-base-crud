c356a90d670d93b2d0633057c37af2389e8c9244
Dennis Empirio <dennis.empirio@gmail.com>
2023-02-02 17:19:07 +0100

    CSS Table ultimato

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,25 +5,28 @@
         <h1>Lista dei Santi</h1>
     </div>
     <ul>
-
+      <div class="container">
+        <table>
+          <tr>
+            <th>Nome</th>
+            <th>Luogo di Nascita</th>
+            <th>Data Benedizione</th>
+            <th>Numero Miracoli</th>
+          </tr>
+        </table>
+      </div>
         @foreach ($saints as $saint)
 
             <div class="container">
-                <table>
-                  <tr>
-                    <th>Nome</th>
-                    <th>Luogo di Nascita</th>
-                    <th>Data Benedizione</th>
-                    <th>Numero Miracoli</th>
-                  </tr>
-                  <tr>
-                    <td>{{ $saint -> name }}</td>
-                    <td>{{ $saint -> birth_place }}</td>
-                    <td>{{ $saint -> blessing_date }}</td>
-                    <td>{{ $saint -> miracles }}</td>
-                  </tr>
-                </table>
-              </div>
+              <table>
+                <tr>
+                  <td>{{ $saint -> name }}</td>
+                  <td>{{ $saint -> birth_place }}</td>
+                  <td>{{ $saint -> blessing_date }}</td>
+                  <td>{{ $saint -> miracles }}</td>
+                </tr>
+              </table>
+            </div>
 
         @endforeach
 
