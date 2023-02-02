bf20bec98fbe98d956e75a2e32479c2174ef1771
Dennis Empirio <dennis.empirio@gmail.com>
2023-02-02 17:14:30 +0100

    Bonus 1 OK, CSS table OK, Bonus 2 not yet

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h1>Santi</h1>
+    <div class="d-flex justify-content-center align-center">
+        <h1>Lista dei Santi</h1>
+    </div>
+    <ul>
+
+        @foreach ($saints as $saint)
+
+            <div class="container">
+                <table>
+                  <tr>
+                    <th>Nome</th>
+                    <th>Luogo di Nascita</th>
+                    <th>Data Benedizione</th>
+                    <th>Numero Miracoli</th>
+                  </tr>
+                  <tr>
+                    <td>{{ $saint -> name }}</td>
+                    <td>{{ $saint -> birth_place }}</td>
+                    <td>{{ $saint -> blessing_date }}</td>
+                    <td>{{ $saint -> miracles }}</td>
+                  </tr>
+                </table>
+              </div>
+
+        @endforeach
+
+    </ul>
 @endsection
